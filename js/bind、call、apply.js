e5a5bf1d9f30989580db6c8fce9e8a5efca53874
xiaoxiaoyan1223 <97817462+xiaoxiaoyan1223@users.noreply.github.com>
2023-10-16 21:43:28 +0800

    Update bind、call、apply.js

--- js/bind、call、apply.js
+++ js/bind、call、apply.js
@@ -24,3 +24,7 @@ function fn1(a,b){
 const fn2=fn1.bind({x:100},10,20)
 const res=fn2()
 console.log(res)
+//都是改变this指向和函数的调用，call和apply的功能类似，只是传参的方法不同call方法传的是一个参数列表
+// apply传递的是一个数组
+// bind传参后不会立刻执行，会返回一个改变了this指向的函数，这个函数还是可以传参的，bind()()
+// call方法的性能要比apply好一些,所以call用的更多一点
