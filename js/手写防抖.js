beeb3bc20b46da19d5e56380a0fe8a84ef372942
xiaoxiaoyan1223 <97817462+xiaoxiaoyan1223@users.noreply.github.com>
2023-10-17 21:48:53 +0800

    Update 手写防抖.js

--- js/手写防抖.js
+++ js/手写防抖.js
@@ -17,3 +17,8 @@ function debounce(fn,delay){
 input1.addEventListener('keyup',debounce(function(){
     console.log(input1.value)
 }),1000)
+// 防抖和节流是什么?
+// 都是应对页面中频繁触发事件的优化方案防抖:避免事件重复触发
+// 使用场景:1.频繁和服务端交互2.输入框的自动保存事件
+// 节流:把频繁触发的事件减少,每隔一段时间执行
+// 使用场景: scroll事件
